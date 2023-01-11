c353e22eb905e8c22d70f0fdf8de9ce75fef923a
Jus <gitgoodjust4lulz@yahoo.com>
2023-01-11 13:14:53 +0100

    implemented json parsing routine and their tests.

--- DBGLogParser.go
+++ DBGLogParser.go
@@ -3,13 +3,25 @@ package DBGLogParser
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"os"
 )
+
 type ERR_NOT_A_TRACE struct{}
+
+type TraceJson struct{
+  Ver int
+  Arch string
+  HashAlgorithm string
+  Hash string
+  Compression string
+  Path string
+}
 
 func (m *ERR_NOT_A_TRACE) Error() string {
   return "Please, provide a trace file"
 }
+
 type ERR_ZERO_TRACE_LENGTH struct{}
 
 func (m *ERR_ZERO_TRACE_LENGTH) Error() string {
@@ -23,27 +35,41 @@ func (m *ERR_JSON_FORMAT_ERROR) Error() string {
 }
 
 // Execute ONLY if there is no possibility to recover from error. If there is, prefer to return error structure instead
-func check(e error)  {
-  if e != nil{
+func check(e error) {
+  if e != nil {
     panic(e)
   }
 }
 
 type stepSnapshot struct {
   instructionMnemonic []string
-  memoryOps []string
-  registerOps []string
+  memoryOps           []string
+  registerOps         []string
 }
 
-type traceCtx struct{
-  registerNames []string
+type traceCtx struct {
+  registerNames  []string
   registerValues []int
-  parsedSteps []stepSnapshot
+  parsedSteps    []stepSnapshot
+  jsonData       *TraceJson
+}
 
+type bBlock struct{
+  blockType uint8
+  registerChanges uint8
+  memoryAccesses uint8
+  blockFlagsAndOpcodeSize uint8
+  threadID uint32
+  opcode []uint8
+  registerChangeNewData []uint32
+  memoryAccessFlags []uint8
+  memoryAccessAddress []uint32
+  memoryAccessOldData []uint32
+  memoryAcessNewData []uint32
 }
 
 func checkMagic(data []byte) (err error) {
-  // Probably in future we will expand list of trace files, so maybe in future we will have to move magic variable to separate list with "magic constants".
+  // Probably in future we will expand list of trace files, so in future we will have to move magic variable to separate list with "magic constants".
   magic := []byte{'T', 'R', 'A', 'C'}
   if !bytes.Equal(data[0:4], magic) {
     err = &ERR_NOT_A_TRACE{}
@@ -53,23 +79,42 @@ func checkMagic(data []byte) (err error) {
 
 func checkJSONLength(data []byte) (length uint32, err error) {
   length = binary.LittleEndian.Uint32(data[4:8])
-  if length==0 {
+  if length == 0 {
     return 0, &ERR_ZERO_TRACE_LENGTH{}
-  } 
+  }
   // check that length of magick + dword(jsonLength) + json is >= length
-  if uint32(len(data)+8) < length || data[length+8]!=byte(0) {
-    // I expect \0 after the end of enclosing bracket
+  if uint32(len(data)+8) < length {
     return length, &ERR_JSON_FORMAT_ERROR{}
   }
-  // If length is not zero and \0 after bracket is present, then assume there is no errors in json
+  // If length is not zero and not less that length of data, then assume there is no errors in json
   return length, nil
 }
 
-func Parse(filename string) (ctx traceCtx, err error){
-  data, err := os.ReadFile(filename) 
+func Parse(filename string) (ctx traceCtx, err error) {
+  data, err := os.ReadFile(filename)
   check(err)
-  if err = checkMagic(data); err != nil {
-    return ctx, err
-  }
+  err = checkMagic(data)
+  check(err) //TOFIX: recover from error
+  length, err := checkJSONLength(data)
+  check(err) //TOFIX: recover from error
+  jsonData := extractJSON(data, length, 8)
+  ctx.jsonData = readJSON(jsonData)
   return ctx, nil
+}
+
+//func parseBinaryTraceBlock(data []byte, blocksParsed int64) (binaryBlock *bBlock) {
+//  //TODO: parse binary block
+//}
+
+func extractJSON(raw []byte, length uint32, offset uint8) (json []byte) {
+  // does not contain \0 at end
+  json = raw[offset:length+uint32(offset)]
+  return json
+}
+
+func readJSON(jsonData []byte) (*TraceJson) {
+  var trace TraceJson
+  err := json.Unmarshal(jsonData, &trace)
+  check(err) //TOFIX: recover from error
+  return &trace
 }
